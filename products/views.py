8e47264fc7df09bf922faffe57e10eea478be2d9
Romario <Sapunovrm@bk.ru>
2025-06-24 13:35:20 +0500

    backend_парсер данных

--- products/views.py
+++ products/views.py
@@ -1,14 +1,22 @@
-from rest_framework import generics
-from .models import Product
-from .serializers import ProductSerializer
+# products/views.py
+from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name='price', lookup_expr='gte')
+    min_rating = filters.NumberFilter(field_name='rating', lookup_expr='gte')
+
+    class Meta:
+        model = Product
+        fields = ['min_price', 'min_rating']
 
 class ProductList(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = {
-        'price': ['gte', 'lte'],
-        'rating': ['gte'],
-        'review_count': ['gte'],
-    }
+    filterset_class = ProductFilter
